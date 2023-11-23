6bb6d75336cc0ac50655302d44d24c1c4f27dd7d
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-11-22 21:57:57 -0500

    1.1.0 - Main File

--- src/main/java/lemon_juice/lemon_core/LemonCore.java
+++ src/main/java/lemon_juice/lemon_core/LemonCore.java
@@ -3,21 +3,20 @@ package lemon_juice.lemon_core;
 import lemon_juice.lemon_core.block.LemonCoreBlocks;
 import lemon_juice.lemon_core.creativetab.LemonCoreCreativeTabs;
 import lemon_juice.lemon_core.item.LemonCoreItems;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(LemonCore.MOD_ID)
 public class LemonCore {
     public static final String MOD_ID = "lemon_core";
 
-    public LemonCore() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public LemonCore(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
 
         // Register Items
         LemonCoreItems.register(modEventBus);
@@ -27,9 +26,7 @@ public class LemonCore {
         LemonCoreCreativeTabs.register(modEventBus);
         modEventBus.addListener(LemonCoreCreativeTabs::registerTabs);
 
-        modEventBus.addListener(this::commonSetup);
-
-        MinecraftForge.EVENT_BUS.register(this);
+        NeoForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
